464922e9aa05171bb05182a1b494c2d35b8f5539
Renato Richard <renatorichard2012@gmail.com>
2020-11-30 20:38:24 -0300

    Tela cadastro

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
-
-  constructor() { }
+  contactField: null;
+  messageField: null;
+  constructor(
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async sendForm(f: NgForm){
+    const message = 'Contato ' + this.contactField + '<br>Mensagem: ' + this.messageField;
+
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message, buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Canceled');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Alerta Confirmado');
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
